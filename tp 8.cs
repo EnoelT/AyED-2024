d805fc02ccb3150b06d6f054728d29af5e448e29
EnoelT <167466177+EnoelT@users.noreply.github.com>
2024-04-18 16:55:16 -0300

    commit

---
+++ tp 8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("edad?");
+            edad = int.Parse(Console.ReadLine());
+
+            int resultado;
+            if (edad < 18)
+            {
+                Console.WriteLine("sos menor");
+            }
+            else if (edad >= 18)
+                Console.WriteLine("sos mayor");
+
+                Console.ReadKey();
+        }
+    }
+}
---
+++ tp9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string clave;
+            clave = "clave1234";
+            Console.WriteLine("contra?");
+
+
+            string clave1;
+            clave1 = Console.ReadLine();
+
+            if (clave == clave1)
+            {
+                Console.WriteLine("es verdad");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            Console.ReadKey();
+        }
+    }
+}
