60f8998df8beabe356ead4056c72a6c2849eb447
EnoelT <167466177+EnoelT@users.noreply.github.com>
2024-05-07 14:19:27 -0300

    subo tp 17, 16 y 10

---
+++ tp 16 tiziano leone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = 0;
+            while (num < 100)
+            {
+                num++;
+
+                if (num % 3 != 0 && num % 5 != 0)
+                {
+                    Console.WriteLine(num);
+                }
+
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+
+                if (num % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+
+                if (num % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
