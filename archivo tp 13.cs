5165f317ba3e486f8e61f6c855b8fdb79bae82f8
EnoelT <167466177+EnoelT@users.noreply.github.com>
2024-05-07 14:30:17 -0300

    subo el 13

---
+++ archivo tp 13.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            Console.WriteLine("palabra que queres que se repita");
+            palabra = (Console.ReadLine());
+
+            Console.WriteLine("aqui tienes:");
+            Console.WriteLine(palabra);
+            Console.WriteLine(palabra);
+            Console.WriteLine(palabra);
+            Console.WriteLine(palabra);
+            Console.WriteLine(palabra);
+            Console.WriteLine(palabra);
+            Console.WriteLine(palabra);
+            Console.WriteLine(palabra);
+            Console.WriteLine(palabra);
+            Console.WriteLine(palabra);
+
+
+            Console.ReadKey();
+        }
+    }
+}
