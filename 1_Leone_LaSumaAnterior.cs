fa0d86e9e7e117e3ef0c44d76fb9d2e52b096b05
EnoelT <167466177+EnoelT@users.noreply.github.com>
2024-10-29 14:59:28 -0300

    Add files via upload

---
+++ 1_Leone_LaSumaAnterior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nro1;
+            Console.WriteLine("numero ?");
+            nro1 = int.Parse(Console.ReadLine());
+
+            int nro2;
+            Console.WriteLine("numero2 ?");
+            nro2 = int.Parse(Console.ReadLine());
+
+            int nro3;
+            Console.WriteLine("numero3 ?");
+            nro3 = int.Parse(Console.ReadLine());
+
+            int resultado;
+            resultado = (nro1 + nro2 + nro3);
+            Console.WriteLine(resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
