6c5d11af36300400925e95c2c2663bd6e9cc4c6c
EnoelT <167466177+EnoelT@users.noreply.github.com>
2024-10-29 15:02:06 -0300

    Add files via upload

---
+++ 11_Leone_ParImpar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int clave1;
+            Console.WriteLine("numero 1");
+            clave1 = int.Parse(Console.ReadLine());
+
+            int clave2;
+            Console.WriteLine("numero2");
+            clave2 = int.Parse(Console.ReadLine());
+
+            if (clave2 / clave1 == 0)
+
+            {
+                Console.WriteLine("error");
+            }
+            else
+            {
+                Console.WriteLine((clave1 / clave2));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5_Leone_ValorDouble.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nro1;
+            Console.WriteLine("ancho ?");
+            nro1 = int.Parse(Console.ReadLine());
+
+            int nro2;
+            Console.WriteLine("largo ?");
+            nro2 = int.Parse(Console.ReadLine());
+
+            int resultado;
+            Console.WriteLine(("suma de los lados ") + (nro1 + nro2 + nro1 + nro2));
+            Console.WriteLine(("area ") + (nro1 * nro2));
+            Console.WriteLine(("diagonal ") + Math.Sqrt(nro1 * nro1 + nro2 * nro2));
+
+
+            Console.ReadKey();
+        }
+    }
+}
