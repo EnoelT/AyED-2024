a596316ad787c4f74011586c53d26d3fd3635958
EnoelT <167466177+EnoelT@users.noreply.github.com>
2024-11-05 16:40:40 -0300

    Add files via upload

---
+++ Etapa 1/3_Leone_DimensionesDeEsfera.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el radio ");
+            circulo(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("ingrese la altura y el radio del cilindro");
+            cilindro(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+
+
+            Console.ReadKey();
+        }
+
+        static double circulo(double radio)
+        {
+            double area = 0;
+            area = 3.14 * (radio * radio);
+            Console.WriteLine("esta es su area : " + area);
+            return area;
+        }
+        static double cilindro(double altura, double area)
+        {
+            double volumen = circulo(area) * altura;
+
+            Console.Clear();
+            Console.Write(" este su volumen : " + volumen);
+            return volumen;
+        }
+    }
+}
