38d0aac23002e7acda962de432a3eea2642e00d3
EnoelT <167466177+EnoelT@users.noreply.github.com>
2024-11-05 14:59:43 -0300

    Add files via upload

---
+++ Etapa 1/0_Leone_CalculadoraBasica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            Console.WriteLine("Numero");
+            valor = int.Parse(Console.ReadLine());
+
+            int valor2;
+
+            Console.WriteLine("Numero 2");
+            valor2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(("suma ") + (valor + valor2));
+            Console.WriteLine(("resta ") + (valor - valor2));
+            Console.WriteLine(("multi ") + (valor * valor2));
+            Console.WriteLine(("divis ") + (valor / valor2));
+            Console.WriteLine(("resto ") + (valor % valor2));
+            Console.ReadKey();
+        }
+    }
+}
