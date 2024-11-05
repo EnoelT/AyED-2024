39d04604e7a6b01f411c616fda6dbea9f3685a59
EnoelT <167466177+EnoelT@users.noreply.github.com>
2024-11-05 16:35:44 -0300

    Add files via upload

---
+++ Etapa 2/12_Leone_RotarMatriz90.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserte valor n y m ");
+            Random randonautica = new Random();
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int[,] MiPrimerTabla = new int[n, m];
+            for (int c = 0; c < m; c++)
+            {
+                for (int f = 0; f < n; f++)
+                {
+
+                    MiPrimerTabla[f, c] = randonautica.Next(0, 100); ;
+                    Console.Write(MiPrimerTabla[f, c] + " \t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            int[,] miSegundaTabla = new int[n, m];
+            for (int c = 0; c < n; c++)
+            {
+                for (int f = 0; f < m; f++)
+                {
+
+                    miSegundaTabla[c, f] = MiPrimerTabla[c, f];
+                    Console.Write(miSegundaTabla[c, f] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Etapa 2/13_Leone_ObtenerDiagonalPrincipal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserte valor n y m ");
+            Random randonautica = new Random();
+            int n = int.Parse(Console.ReadLine());
+
+            int[] vector = new int[n];
+            int[,] MiPrimerTabla = new int[n, n];
+            for (int c = 0; c < n; c++)
+            {
+                for (int f = 0; f < n; f++)
+                {
+
+                    MiPrimerTabla[f, c] = randonautica.Next(0, 100); ;
+                    Console.Write(MiPrimerTabla[f, c] + "\t");
+
+                }
+                Console.WriteLine();
+
+
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                vector[i] = MiPrimerTabla[i, i];
+                Console.Write(vector[i] + "\t" + "\n");
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
