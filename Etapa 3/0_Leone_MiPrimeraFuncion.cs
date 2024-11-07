a77000d91711c1d61c05762c01d1372edce8b690
EnoelT <167466177+EnoelT@users.noreply.github.com>
2024-11-07 14:23:37 -0300

    tp 0 con .cs, falta el 1

---
+++ Etapa 3/0_Leone_MiPrimeraFuncion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿cual es tu nombre?");
+            M(Console.ReadLine());
+
+
+
+            Console.ReadKey();
+        }
+        static string M(string nombre)
+        {
+            Console.WriteLine("¡Hola " + nombre + " !");
+            return nombre;
+
+        }
+    }
+}
+    }
+}
